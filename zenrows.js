a5cb3a8c9d7aafead18c915882a28a3afbeeb105
monsur22 <monsurahmedshafiq@gmail.com>
2022-02-01 14:55:23 +0600

    click button i accept

--- zenrows.js
+++ zenrows.js
@@ -10,6 +10,11 @@ async function main() {
 
     const page = await browser.newPage();
     await page.goto('https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/ od-2014/q-actros? search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at %3Adesc');
+    await page.click('text=Akceptuję');
+    // await page.waitForFunction(() => {
+    //     const repoCards = document.querySelectorAll('article.border');
+    //     return repoCards.length > 30;
+    // });
     await page.waitForTimeout(5000); // wait for 5 seconds
     await browser.close();
 }
