5df6d759840ec99b721967ec424c56f48aa7c358
monsur22 <monsurahmedshafiq@gmail.com>
2022-02-01 19:39:57 +0600

    1, 2 , 3 , 4 complete without id collection

---
+++ presidant.js
@@ -0,0 +1,26 @@
+const playwright = require('playwright');
+async function mostActive() {
+    const browser = await playwright.chromium.launch({
+        headless: true // set this to true
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://finance.yahoo.com/most-active?count=100');
+    const mostActive = await page.$eval('#fin-scr-res-table tbody', tableBody => {
+        let all = []
+        for (let i = 0, row; row = tableBody.rows[i]; i++) {
+            let stock = [];
+            for (let j = 0, col; col = row.cells[j]; j++) {
+                stock.push(row.cells[j].innerText)
+            }
+            all.push(stock)
+        }
+        return all;
+    });
+
+    console.log('Most Active', mostActive);
+    await page.waitForTimeout(30000); // wait
+    await browser.close();
+}
+
+mostActive();
--- zenrows.js
+++ zenrows.js
@@ -11,6 +11,7 @@ async function main() {
     const page = await browser.newPage();
     await page.goto('https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/ od-2014/q-actros? search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at %3Adesc');
     await page.click('text=Akceptuję');
+    await page.click('.ooa-1xwoou1');
     // await page.waitForFunction(() => {
     //     const repoCards = document.querySelectorAll('article.border');
     //     return repoCards.length > 30;
