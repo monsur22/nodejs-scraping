a60a59162596564ea2cd095e99785a8448ec9cf5
monsur22 <monsurahmedshafiq@gmail.com>
2022-02-01 14:12:47 +0600

    initial commit

---
+++ zenrows.js
@@ -0,0 +1,17 @@
+// const axios = require('axios');
+// axios.get('https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/ od-2014/q-actros? search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at %3Adesc')
+// 	.then(({ data }) => console.log(data));
+
+const playwright = require('playwright');
+async function main() {
+    const browser = await playwright.chromium.launch({
+        headless: false // setting this to true will not run the UI
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/ od-2014/q-actros? search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at %3Adesc');
+    await page.waitForTimeout(5000); // wait for 5 seconds
+    await browser.close();
+}
+
+main();
