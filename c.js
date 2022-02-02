25a4d4083007681c2f87bd4a44cbf6329e56acbb
monsur22 <monsurahmedshafiq@gmail.com>
2022-02-02 20:01:38 +0600

    remove practice file

--- c.js
+++
@@ -1,8 +0,0 @@
-const cheerio = require('cheerio');
-const $ = cheerio.load('<h2 class="title">ScrapingAnt is awesome!</h2>');
-
-$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
-
-$.html();
-console.log($.html());
